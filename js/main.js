7980af41161edebe171f0d1af70a409d3d1bb489
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2024-11-07 14:37:20 +0700

    add: modal window

--- js/main.js
+++ js/main.js
@@ -122,3 +122,21 @@ const swiperSteps = new Swiper('.swiper-steps', {
       },
     },
   });
+
+// Modal START
+const modal = document.querySelector('.modal')
+const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+const modalClose = document.querySelector('.modal-close');
+console.log(modalToggle);
+modalToggle.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.add('is-open');
+  });
+});
+modalClose.addEventListener('click', (event) => {
+event.preventDefault();
+modal.classList.remove('is-open');
+});
+
+  
