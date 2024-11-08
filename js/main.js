7423b9c9b9864f23ac698bc0aa2eeeb3ab3eaa61
ilyuhin6 <142712102+ilyuhin6@users.noreply.github.com>
2024-11-08 07:17:49 +0700

    fix: modal script js

--- js/main.js
+++ js/main.js
@@ -125,22 +125,45 @@ const swiperSteps = new Swiper('.swiper-steps', {
 
 // Modal START
 const modal = document.querySelector('.modal')
-const modalToggle = document.querySelectorAll('[data-toggle=modal]');
-const modalClose = document.querySelector('.modal-close');
-console.log(modalToggle);
-modalToggle.forEach((element) => {
-  element.addEventListener('click', (event) => {
+const modalDialog = document.querySelector('.modal-dialog');
+
+document.addEventListener('click', (event) => {
+  if (
+    event.target.dataset.toggle == 'modal' ||
+    event.target.parentNode.dataset.toggle == 'modal' ||
+    (!event.composedPath().includes(modalDialog) &&
+    modal.classList.contains('is-open'))
+  ) {
     event.preventDefault();
-    modal.classList.add('is-open');
-  });
-});
-modalClose.addEventListener('click', (event) => {
-event.preventDefault();
-modal.classList.remove('is-open');
-});
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-      modal.classList.remove('is-open');
+    modal.classList.toggle('is-open');
   }
 });
+document.addEventListener('keyup', (event) => {
+  if (event.key == 'Escape' && modal.classList.contains('is-open')) {
+    modal.classList.toggle('is-open');
+  }
+});
+
+
+
+
+
+// const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+// const modalClose = document.querySelector('.modal-close');
+// console.log(modalToggle);
+// modalToggle.forEach((element) => {
+//   element.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     modal.classList.add('is-open');
+//   });
+// });
+// modalClose.addEventListener('click', (event) => {
+// event.preventDefault();
+// modal.classList.remove('is-open');
+// });
+// modal.addEventListener('click', (event) => {
+//   if (event.target === modal) {
+//       modal.classList.remove('is-open');
+//   }
+// });
   
